23c76d47b09160082560e8fc352f4e205163e749
MatthiasChometon <matthiaschometon787@gmail.com>
2022-03-06 14:53:30 +0100

    feat: add all_weather, forecast_weather, meteo and position models

---
+++ lib/models/all_weather.dart
@@ -0,0 +1,8 @@
+import 'package:sunisup/models/forecast_weather.dart';
+import 'package:sunisup/models/meteo.dart';
+
+class All_weather {
+  final Meteo meteo;
+  final ForecastWeather forecastWeather;
+  All_weather(this.meteo, this.forecastWeather);
+}
