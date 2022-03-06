634148a6fc206d3c8885bd90c344ca7707e29dbf
MatthiasChometon <matthiaschometon787@gmail.com>
2022-03-06 14:52:49 +0100

    feat: add global text size

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sun Is Up',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        textTheme: Theme.of(context).textTheme.apply(
+              fontSizeFactor: 0.8,
+              fontSizeDelta: 2.0,
+            ),
         primarySwatch: Colors.blue,
       ),
       home: const Home(title: 'Sun Is Up'),
