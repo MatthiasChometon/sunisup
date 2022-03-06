89a7ce7b2ad5a9bac949ea8f7ea638d86815f599
MatthiasChometon <matthiaschometon787@gmail.com>
2022-03-06 14:52:16 +0100

    feat: add datepanel, meteopanel and weatherpanel models

---
+++ lib/widgets/MeteoPanel.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+
+class MeteoPanel extends StatefulWidget {
+  const MeteoPanel(
+      {Key? key, required this.temp, required this.humidity, required this.deg})
+      : super(key: key);
+
+  final String temp;
+  final String humidity;
+  final String deg;
+
+  @override
+  _MeteoPanelState createState() {
+    return _MeteoPanelState();
+  }
+}
+
+class _MeteoPanelState extends State<MeteoPanel> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(children: [
+        const Icon(
+          Icons.thermostat,
+          color: Colors.black,
+          size: 24.0,
+        ),
+        Text(
+          "${widget.temp}°",
+          style: const TextStyle(color: Colors.black),
+        ),
+      ]),
+      Row(children: [
+        const Icon(
+          Icons.water_outlined,
+          color: Colors.black,
+          size: 24.0,
+        ),
+        Text(
+          "${widget.humidity}%",
+          style: const TextStyle(color: Colors.black),
+        ),
+      ]),
+      Row(children: [
+        const Icon(
+          CupertinoIcons.wind,
+          color: Colors.black,
+          size: 24.0,
+        ),
+        Text(
+          "${widget.deg}km/h",
+          style: const TextStyle(color: Colors.black),
+        ),
+      ])
+    ], crossAxisAlignment: CrossAxisAlignment.start);
+  }
+}
