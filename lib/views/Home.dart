1102bfbd5c03ecf2b40bb9fbcaaa08137005b920
MatthiasChometon <matthiaschometon787@gmail.com>
2022-03-24 11:10:28 +0100

    feat: add background picture

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -16,60 +16,79 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  String meteoWallpaper = "assets/wallpapers/Clear.jpg";
 
   @override
   Widget build(BuildContext context) {
+    void getWeatherWallpaper(meteo) {
+      setState(() {
+        meteoWallpaper = "assets/wallpapers/$meteo.jpg";
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
+          backgroundColor: const Color.fromRGBO(63, 193, 201, 1),
+          centerTitle: true,
+          title: Text(widget.title),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('assets/icons/sunIsUp_white.png',
+                fit: BoxFit.cover),
+          )),
       body: FutureBuilder<List<All_weather>>(
           future: getAllMeteoInDatabase(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(child: Text("Chargement en cours ... "));
             } else if (snapshot.connectionState == ConnectionState.done) {
-              return SizedBox(
-                  height: 600,
-                  child: ReorderableListView(
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.symmetric(horizontal: 40),
-                    children: <Widget>[
-                      for (int index = 0;
-                          index < snapshot.data!.length;
-                          index += 1)
-                        WeatherPanel(
-                            key: Key('$index'),
-                            meteo: snapshot.data![index].meteo,
-                            forecastWeather:
-                                snapshot.data![index].forecastWeather)
-                    ],
-                    onReorder: (int oldIndex, int newIndex) {
-                      if (oldIndex < newIndex) {
-                        newIndex -= 1;
-                      }
-                      setState(() {
-                        final All_weather item =
-                            snapshot.data!.removeAt(oldIndex);
-                        snapshot.data!.insert(newIndex, item);
-                      });
-                    },
-                  ));
+              meteoWallpaper =
+                  "assets/wallpapers/${snapshot.data![0].meteo.weather![0].main}.jpg";
+              return Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(meteoWallpaper),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: ReorderableListView(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      children: <Widget>[
+                        for (int index = 0;
+                            index < snapshot.data!.length;
+                            index += 1)
+                          WeatherPanel(
+                              key: Key('$index'),
+                              meteo: snapshot.data![index].meteo,
+                              forecastWeather:
+                                  snapshot.data![index].forecastWeather)
+                      ],
+                      onReorder: (int oldIndex, int newIndex) {
+                        if (oldIndex < newIndex) {
+                          newIndex -= 1;
+                        }
+                        setState(() {
+                          getWeatherWallpaper(
+                              snapshot.data![0].meteo.weather![0].main);
+                          final All_weather item =
+                              snapshot.data!.removeAt(oldIndex);
+                          snapshot.data!.insert(newIndex, item);
+                        });
+                      },
+                    )),
+              );
             } else {
               return const Text("Une erreur est survenue");
             }
           }),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        backgroundColor: const Color.fromRGBO(63, 193, 201, 1),
+        onPressed: () => {},
+        tooltip: 'add city',
         child: const Icon(Icons.add),
       ),
     );
